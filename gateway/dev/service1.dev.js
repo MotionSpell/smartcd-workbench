e564e6e6bdb4a5940b3163cd8f342e177a48b8d0
Nils <nils.dvl+git@gmail.com>
2025-11-20 13:23:31 +0100

    make orchestrator related variable configurable

--- gateway/dev/service1.dev.js
+++ gateway/dev/service1.dev.js
@@ -1,11 +1,9 @@
 import {XMLHttpRequest} from 'xhr';
-// import {now} from 'os';
 
-
-// SERVICE ID / SPECIFIC INFORMATION ? 
-const ORCHESTRATOR_URI =  "http://127.0.0.1:3000/api";
-const SOFTWARE_RELEASE_URI = "urn:1b54fdfa-55da-4896-9f53-028318ad51b5";
-const COMPUTER_GUID = "COMP-0";
+// TODO: get this from external config file
+let ORCHESTRATOR_URI =  "http://127.0.0.1:3000/api";
+let SOFTWARE_RELEASE_URN = "urn:1b54fdfa-55da-4896-9f53-028318ad51b5"; // service specific ?
+let COMPUTER_GUID = "COMP-0"; // service specific ?
 
 let SERVICE_CONFIG = null;
 let polling = false;
@@ -34,13 +32,11 @@ function slaposFindMabrServerInstanceID(){
                         for (let res of results){
                             if (res["state"] == "started"){ ///!\ assumes we just need the 1st one ...
                                 instanceMabrServerInstanceGuid = res["instance_guid"];
-                                print(GF_LOG_WARNING, `[js] service1 instance guid: ${instanceMabrServerInstanceGuid}`);
+                                // print(GF_LOG_WARNING, `[js] service1 instance guid: ${instanceMabrServerInstanceGuid}`);
                                 return slaposGetMabrServerAddress(instanceMabrServerInstanceGuid);
                             }
                         }
-                    } else {
-                        print(GF_LOG_WARNING, `[js] slaposFindMabrServerInstanceID.onload - Invalid response payload`);
-                    }
+                    } 
                 } catch (err) {
                         print(GF_LOG_WARNING, `[js] slaposFindMabrServerInstanceID.onload - Invalid response payload: ${err}`);
                 }
@@ -48,25 +44,20 @@ function slaposFindMabrServerInstanceID(){
             instanceMabrServerAddress = null;
             instanceMabrServerInstanceGuid = null;
          }
-        print(GF_LOG_WARNING, `[js] slaposFindMabrServerInstanceID.onload - ${req.readyState} - ${req.status} - ${req.statusText}`);
         polling = false;
     };
     req.onerror = (e) => { polling = false; print(GF_LOG_WARNING, `[js] slaposFindMabrServerInstanceID.onerror - ${req.readyState} - ${req.status} - ${req.statusText}`) };
     const msg = JSON.stringify({
-        "software_release_uri": SOFTWARE_RELEASE_URI,
+        "software_release_uri": SOFTWARE_RELEASE_URN,
         "computer_guid": COMPUTER_GUID
     });
-    // print(GF_LOG_WARNING, `[js] slaposFindMabrServerInstanceID - ${uri}`);
     polling = true;
     req.send(msg);
 }
 
-
 function slaposGetMabrServerAddress(instanceGuid){
 
     const uri = `${ORCHESTRATOR_URI}/slapos.get.v0.software_instance?cacheburst=${cacheBurst()}`;
-    print(GF_LOG_WARNING, `[js] slaposGetMabrServerAddress - ${uri}`);
-
     const req = new XMLHttpRequest();
     req.open("POST", uri, false);
     req.setRequestHeader("Content-Type", "application/json");
@@ -101,11 +92,7 @@ function slaposGetMabrServerAddress(instanceGuid){
 }
 
 function pollForMabrServerDetails(){
-    print(GF_LOG_WARNING, `[js] pollForMabrServerDetails - polling: ${polling}`);
-    print(GF_LOG_WARNING, `[js] instanceMabrServerInstanceGuid: ${instanceMabrServerInstanceGuid}`);
-    print(GF_LOG_WARNING, `instanceMabrServerAddress: ${instanceMabrServerAddress}`);
     if(polling) return;
-    // how do we check for end of mABR availability ?
     if (instanceMabrServerInstanceGuid) {
         slaposGetMabrServerAddress(instanceMabrServerInstanceGuid);
     } else {
@@ -115,46 +102,31 @@ function pollForMabrServerDetails(){
     }
 }
 
-/*
-function setInterval(callback, delay){
-    setTimeout(()=>{
-        callback();
-        setInterval(callback, delay);
-    }, delay);
-}
-
-let count = 0;
-
-function intervalCallback(){
-    count += 1;
-    print(GF_LOG_INFO, `[js] intervalCallback ${count}`);
-}
-*/
-
 export function init(service_config) 
 {
-    SERVICE_CONFIG = service_config;    
+    SERVICE_CONFIG = service_config;
+    ORCHESTRATOR_URI = service_config.smartcd_api_endpoint;
+    SOFTWARE_RELEASE_URN = service_config.smartcd_service_software_release_urn;
+    COMPUTER_GUID = service_config.smartcd_service_computer_guid;
     return true;
 }
 
-
 export function service_activation(do_load, serviceID)
 {
-    print(GF_LOG_INFO, `[js] service_activation - do_load:${do_load}, serviceID:${serviceID}`);
+    // print(GF_LOG_INFO, `[${serviceID}] service_activation - do_load:${do_load}`);
     return null;
 }
 
 // FIXME: the function is not optional
 export function quality_activation(do_activate, serviceID, periodID, adaptationSetID, representationID)
 {
-    print(GF_LOG_INFO, `[js] quality_activation - do_activate:${do_activate}, serviceID:${serviceID}, periodID:${periodID}, adaptationSetID:${adaptationSetID}, representationID:${representationID}`);
+    // print(GF_LOG_INFO, `[${serviceID}] quality_activation - do_activate:${do_activate}, periodID:${periodID}, adaptationSetID:${adaptationSetID}, representationID:${representationID}`);
     return true;
 }
 
 export function get_mcast_address(service_url)
 {
     pollForMabrServerDetails();
-    print(GF_LOG_INFO, `[js] get_mcast_address: ${service_url} - ${instanceMabrServerInstanceGuid} - ${instanceMabrServerAddress}`);
+    // print(GF_LOG_INFO, `get_mcast_address: ${service_url} - ${instanceMabrServerInstanceGuid} - ${instanceMabrServerAddress}`);
     return instanceMabrServerAddress;
 }
-
