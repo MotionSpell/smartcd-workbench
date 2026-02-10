7079a18655df9a55c3a7bf325ceeaa6c95231053
Nils <nils.dvl+git@gmail.com>
2026-02-10 14:38:23 +0100

    update gateway's config

--- scripts/client/mabr-gateway.sh
+++ scripts/client/mabr-gateway.sh
@@ -2,4 +2,4 @@ DIR=/data/data/com.termux/files/home/smartcd-workbench
 mkdir -p $DIR/tmp/mabr-gateway
 rm -rf $DIR/tmp/mabr-gateway/*
 
-gpac -log=all@error:http:route@info -log-file=$DIR/logs/mabr-gateway.log -tmp=$DIR/tmp/mabr-gateway mediaserver=$DIR/scripts/client/gateway.scfg
+gpac -log=all@error:http:route@info -log-file=$DIR/logs/mabr-gateway.log -tmp=$DIR/tmp/mabr-gateway mediaserver:port=8081:scfg=$DIR/scripts/client/gateway.scfg
