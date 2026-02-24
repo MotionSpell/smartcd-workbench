16defb6d72a9dd5f2a75ce4cedd9694a4f92b7ea
Nils <nils.dvl+git@gmail.com>
2026-02-24 08:46:16 +0100

    update termux cfg

--- cfg/termux/start-gateway.sh
+++
@@ -1,4 +0,0 @@
-DIR=/data/data/com.termux/files/home/smartcd-workbench
-mkdir -p $DIR/tmp/gateway
-rm -rf $DIR/tmp/gateway/*
-gpac -logs=all@error:http:route:console:script@info -log-file=$DIR/logs/gateway.log -tmp=$DIR/tmp/gateway mediaserver:port=8081:scfg=$DIR/cfg/local/route.scfg
